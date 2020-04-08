aa16e96df24ca5581d655ee329e5984554c84e86
funnywus <312376873@qq.com>
2020-04-08 13:24:43 +0800

    first commit

---
+++ src/main/java/com/funny/git/Apple.java
@@ -0,0 +1,8 @@
+package com.funny.git;
+
+/**
+ * @author funnywu5 email: funnywu5@162.com
+ * @create 2020-04-08 12:46
+ */
+public class Apple {
+}
---
+++ src/test/java/com/funny/git/test/TestGit.java
@@ -0,0 +1,8 @@
+package com.funny.git.test;
+
+/**
+ * @author funnywu5 email: funnywu5@162.com
+ * @create 2020-04-08 12:48
+ */
+public class TestGit {
+}
